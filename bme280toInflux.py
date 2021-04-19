bccd1382f4311aa2f624fe7ca76bd1fca83bdf47
agluck28 <agluck28@gmail.com>
2021-04-19 09:49:10 -0700

    Latest changes from testing

--- bme280toInflux.py
+++ bme280toInflux.py
@@ -1,4 +1,7 @@
-import time, board, busio, adafruit_bme280, json
+#!/home/pi/Documents/coding/GIT/python-bme280/env/bin/python
+import sys
+import time, smbus2, bme280, json
+sys.path.append('/home/pi/Documents/coding/GIT/modules')
 from Flux.Flux import Writer
 from PointWriter.PointWriter import PointWriter
 
@@ -10,15 +13,18 @@ This is setup to send data at a rate of roughly once per minute
 '''
 
 #read constants from configuration file
-with open('./.configuration/db_settings.json.local') as jsondata:
+with open('/home/pi/Documents/coding/GIT/python-bme280/.configuration/db_settings.local.json') as jsondata:
     config = json.load(jsondata)
 
 #open up a writer connection
-db_writer = Writer(config['token'], config['org'], config['bucket'], config['url'])
+db_writer = Writer(config['token'], config['org'], config['url'], config['bucket'])
 
 #open connection to the sensor on the board
-i2c = busio.I2C(board.SCL, board.SDA)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+port = 1
+address = 0x77
+bus = smbus2.SMBus(port)
+
+calibration_params = bme280.load_calibration_params(bus, address)
 
 #initialize data dict
 data = {
@@ -30,10 +36,12 @@ data = {
 base_point = PointWriter(config['roomId'],('deviceId', config['deviceId']))
 
 while True:
-        data['temperature'] = (bme280.temperature * 9/5) +32
-        data['humidity'] = bme280.humidity
+        test = bme280.sample(bus, address, calibration_params)
+        data['temperature'] = (test.temperature * 9/5) +32
+        data['humidity'] = test.humidity
         #check if valid value, and if not, try again
         if (20 < data['temperature'] < 100) and (0 < data['humidity'] < 100):
                 temp_point = base_point.add_fields(data, time.time_ns())
                 db_writer.write_data(temp_point)
                 time.sleep(60)
+                
