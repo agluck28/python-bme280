81af469b80a69438a031a36ca1b6c65dad2885ad
agluck28 <agluck@gmail.com>
2021-04-19 10:22:06 -0700

    Update bme280toInflux.py
    Switches to use rabbitmq

--- bme280toInflux.py
+++ bme280toInflux.py
@@ -1,9 +1,11 @@
 #!/home/pi/Documents/coding/GIT/python-bme280/env/bin/python
+from RabbitHelper.RabbitHelper import Rabbit
 import sys
-import time, smbus2, bme280, json
+import time
+import smbus2
+import bme280
+import json
 sys.path.append('/home/pi/Documents/coding/GIT/modules')
-from Flux.Flux import Writer
-from PointWriter.PointWriter import PointWriter
 
 
 '''
@@ -12,36 +14,39 @@ on a raspberry PI and send the data to an influxDB instance in the cloud
 This is setup to send data at a rate of roughly once per minute
 '''
 
-#read constants from configuration file
+# read constants from configuration file
 with open('/home/pi/Documents/coding/GIT/python-bme280/.configuration/db_settings.local.json') as jsondata:
     config = json.load(jsondata)
 
-#open up a writer connection
-db_writer = Writer(config['token'], config['org'], config['url'], config['bucket'])
+#setup routing key, needs to be a list
+key = [f"bme280.{config['roomId']}.{config['deviceId']}"]
 
-#open connection to the sensor on the board
+# open up a Rabbit connection
+rabbit = Rabbit(config['url'], config['exchange'], key)
+
+# open connection to the sensor on the board
 port = 1
 address = 0x77
 bus = smbus2.SMBus(port)
 
 calibration_params = bme280.load_calibration_params(bus, address)
 
-#initialize data dict
+# initialize data dict
 data = {
+    'time': time.time_ns(),
+    'measurements': {
         'temperature': -1,
         'humidity': -1
+    }
 }
 
-#setup base point
-base_point = PointWriter(config['roomId'],('deviceId', config['deviceId']))
 
 while True:
-        test = bme280.sample(bus, address, calibration_params)
-        data['temperature'] = (test.temperature * 9/5) +32
-        data['humidity'] = test.humidity
-        #check if valid value, and if not, try again
-        if (20 < data['temperature'] < 100) and (0 < data['humidity'] < 100):
-                temp_point = base_point.add_fields(data, time.time_ns())
-                db_writer.write_data(temp_point)
-                time.sleep(60)
-                
+    test = bme280.sample(bus, address, calibration_params)
+    data['measurements']['temperature'] = (test.temperature * 9/5) + 32
+    data['measurements'] = test.humidity
+    # check if valid value, and if not, try again
+    if (20 < data['temperature'] < 100) and (0 < data['humidity'] < 100):
+        data['time'] = time.time_ns()
+        rabbit.send_message(data)
+        time.sleep(60)
